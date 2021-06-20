fe9af69402fb5597afef0e9fba7b3058d2ea7746
melodiczzz <melodicz@protonmail.com>
2021-06-21 01:52:30 +0600

    final link satisfied

--- android_adb_alternative.sh
+++ android_adb_alternative.sh
@@ -2,6 +2,9 @@
 #Todo --- Take this output as a variable
 # mv 
 OUTPUT=$(curl --upload-file ./app/build/outputs/apk/debug/app-debug.apk https://transfer.sh/app.zip)
+CODE=$(echo "$OUTPUT" | sed 's/.*sh//; s/app.*//' | sed 's/\(^.\)\|\(.$\)//g')
+OUTPUT="https://transfer.sh/get/$CODE/app.zip"
+
 curl -X POST \
 -H "X-Parse-Application-Id: 96IF9A8Dqw4cSdJo4LgdTkdD9s2EziWKyf61hUPk" \
 -H "X-Parse-REST-API-Key: NAjHO2RUsJrvCYTpcKhD3koNKYawDCpIHWgPd2nT" \
